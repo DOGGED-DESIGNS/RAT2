a991d9a13fd507d6eecef6754a2ee46296ed7c24
dogged <uzoechijerry@gmail.com>
2025-04-08 16:35:26 +0100

    examination

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Maxwidthwrapper from "@/components/Maxwidthwrapper";
 import Movinbg from "@/components/Movinbg";
 import Navbar from "@/components/Navbar";
+
 import { AuroraBackground } from "@/components/ui/Arorabg";
 import { motion } from "framer-motion";
 
@@ -8,8 +9,43 @@ import Image from "next/image";
 import { BackgroundBeamsDemo } from "@/components/Blackbg";
 import { Button } from "@/components/ui/button";
 import { CardHoverEffectDemo } from "@/components/Cards";
+import { MovingBorderDemo } from "@/components/Moving";
+import { InfiniteMovingCards } from "@/components/ui/Infinite-moving";
+import { Spotlight } from "@/components/ui/Spotlight";
+import { WobbleCard } from "@/components/ui/Wobble-card";
 
 export default function Home() {
+  const testimonials = [
+    {
+      quote:
+        "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair.",
+      name: "Charles Dickens",
+      title: "A Tale of Two Cities",
+    },
+    {
+      quote:
+        "To be, or not to be, that is the question: Whether 'tis nobler in the mind to suffer The slings and arrows of outrageous fortune, Or to take Arms against a Sea of troubles, And by opposing end them: to die, to sleep.",
+      name: "William Shakespeare",
+      title: "Hamlet",
+    },
+    {
+      quote: "All that we see or seem is but a dream within a dream.",
+      name: "Edgar Allan Poe",
+      title: "A Dream Within a Dream",
+    },
+    {
+      quote:
+        "It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a wife.",
+      name: "Jane Austen",
+      title: "Pride and Prejudice",
+    },
+    {
+      quote:
+        "Call me Ishmael. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world.",
+      name: "Herman Melville",
+      title: "Moby-Dick",
+    },
+  ];
   return (
     <>
       <Maxwidthwrapper newClass=" ">
@@ -24,7 +60,7 @@ export default function Home() {
 
         {/* licensed investment adviser */}
 
-        <div className=" bg-purple-200">
+        <div className=" bg-purple-100">
           <div className=" py-8 grid max-w-screen-md  w-full m-auto gap-10 grid-cols-2">
             <div className=" ">
               <div className=" rounded-lg bg-zinc-500  max-w-lg  h-[500px]">
@@ -79,6 +115,99 @@ export default function Home() {
         {/* this is the card section */}
 
         <CardHoverEffectDemo />
+
+        {/* this is another section */}
+        <div className="   px-5 py-5  m-auto bg-slate-900   mt-9">
+          <div className=" mx-auto max-w-screen-lg">
+            <div>
+              <div className=" mt-5 mb-12">
+                <h6 className="  text-purple-500 text-sm font-bold">
+                  My Experience
+                </h6>
+                <h1 className=" mt-3 text-white font-bold text-4xl ">
+                  My Work Experience
+                </h1>
+              </div>
+
+              <div>
+                <MovingBorderDemo />
+              </div>
+            </div>{" "}
+          </div>
+        </div>
+
+        {/* this is the infinit movin card */}
+        <div className="  mx-auto h-[30rem] rounded-md flex flex-col antialiased bg-slate-200 dark:bg-black dark:bg-grid-white/[0.05] items-center justify-center relative overflow-hidden">
+          <Spotlight />
+          <div className=" max-w-screen-lg  mx-auto ">
+            <div className=" mb-10">
+              <h6 className=" text-sm font-bold text-purple-950">Rita oroto</h6>
+              <h2 className=" text-4xl font-bold ">Client Review</h2>
+            </div>
+            <InfiniteMovingCards
+              items={testimonials}
+              direction="right"
+              speed="slow"
+            />
+          </div>
+        </div>
+
+        {/* brokage examination */}
+
+        <div className=" mt-10 py-6  ">
+          <div className=" max-w-screen-lg mx-auto ">
+            <h2 className=" text-sm text-center font-bold text-purple-900">
+              Rita oroto
+            </h2>
+            <h1 className=" text-4xl text-center font-bold mt-4">
+              Brokage Examination
+            </h1>
+            <div className="w-full mt-10 gap-10 grid grid-cols-3">
+              <WobbleCard containerClassName=" col-span-1 bg-slate-200  ">
+                <div>
+                  <h2 className=" font-bold text-lg  capitalize">
+                    Security Entrance Examination
+                  </h2>
+                  <div className=" h-[2px] my-2 w-full bg-slate-300 " />
+
+                  <h2 className=" text-slate-600 font-bold text-base mt=5">
+                    General Industry/ Product Examination
+                  </h2>
+                </div>
+              </WobbleCard>
+              <WobbleCard containerClassName=" col-span-1 bg-pink-200  ">
+                <div>
+                  <h2 className=" font-bold text-lg  capitalize">
+                    Security Entrance Examination
+                  </h2>
+                  <div className=" h-[2px] my-2 w-full bg-slate-300 " />
+
+                  <h2 className=" text-slate-600 font-bold text-base mt=5">
+                    General Industry/ Product Examination
+                  </h2>
+                </div>
+              </WobbleCard>
+              <WobbleCard containerClassName=" col-span-1 bg-purple-200  ">
+                <div>
+                  <h2 className=" font-bold text-lg  capitalize">
+                    Security Entrance Examination
+                  </h2>
+                  <div className=" h-[2px] my-2 w-full bg-slate-300 " />
+
+                  <h2 className=" text-slate-600 font-bold text-base mt=5">
+                    General Industry/ Product Examination
+                  </h2>
+                </div>
+              </WobbleCard>
+            </div>
+          </div>
+        </div>
+
+        {/* form section */}
+        <div> contact section </div>
+
+        {/* fotter section */}
+        <div> this is the footer section </div>
       </Maxwidthwrapper>
     </>
   );
@@ -91,3 +220,5 @@ export default function Home() {
 // card hover effect
 // movingbar
 // spotlight new
+
+// infinit moving card
---
+++ src/components/Moving.tsx
@@ -0,0 +1,79 @@
+"use client";
+import React from "react";
+// import { Button } from "./ui/moving-border";
+import { Button } from "./ui/Movingborder";
+
+export function MovingBorderDemo() {
+  return (
+    <div className=" mt-6 grid grid-cols-2  gap-10 max-w-screen-lg m-auto ">
+      <Button
+        borderRadius="0.75rem"
+        duration={Math.floor(Math.random() * 10000 + 10000)}
+        classNames="    dark:border-slate-800"
+      >
+        <div className="   p-5 ">
+          <h3 className=" text-left text-lg text-zinc-600">
+            From 01/2018 - present
+          </h3>
+          <h2 className="   text-left text-zinc-100 mt-3 font-medium text-lg">
+            Check Pdf for more details
+          </h2>
+          <h1 className="text-left text-lg mt-3 font-medium capitalize text-purple-800">
+            Financial Adviser
+          </h1>
+        </div>
+      </Button>
+      <Button
+        borderRadius="0.75rem"
+        duration={Math.floor(Math.random() * 10000 + 10000)}
+        classNames="    dark:border-slate-800"
+      >
+        <div className="   p-5 ">
+          <h3 className=" text-left text-lg text-zinc-600">
+            From 01/2018 - present
+          </h3>
+          <h2 className="   text-left text-zinc-100 mt-3 font-medium text-lg">
+            Check Pdf for more details
+          </h2>
+          <h1 className="text-left text-lg mt-3 font-medium capitalize text-purple-800">
+            Financial Adviser
+          </h1>
+        </div>
+      </Button>
+      <Button
+        borderRadius="0.75rem"
+        duration={Math.floor(Math.random() * 10000 + 10000)}
+        classNames="    dark:border-slate-800"
+      >
+        <div className="   p-5 ">
+          <h3 className=" text-left text-lg text-zinc-600">
+            From 01/2018 - present
+          </h3>
+          <h2 className="   text-left text-zinc-100 mt-3 font-medium text-lg">
+            Check Pdf for more details
+          </h2>
+          <h1 className="text-left text-lg mt-3 font-medium capitalize text-purple-800">
+            Financial Adviser
+          </h1>
+        </div>
+      </Button>
+      <Button
+        borderRadius="0.75rem"
+        duration={Math.floor(Math.random() * 10000 + 10000)}
+        classNames="    dark:border-slate-800"
+      >
+        <div className="   p-5 ">
+          <h3 className=" text-left text-lg text-zinc-600">
+            From 01/2018 - present
+          </h3>
+          <h2 className="   text-left text-zinc-100 mt-3 font-medium text-lg">
+            Check Pdf for more details
+          </h2>
+          <h1 className="text-left text-lg mt-3 font-medium capitalize text-purple-800">
+            Financial Adviser
+          </h1>
+        </div>
+      </Button>
+    </div>
+  );
+}
