3b223f5c20eecad55d766e22b9ce4b08a1cc7be3
dogged <uzoechijerry@gmail.com>
2025-04-11 18:57:35 +0100

    finalhopful

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+const siteUrl = "https://www.ritaorow.com";
+module.exports = {
+  siteUrl,
+  generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [{ userAgent: "*", allow: "/" }],
+  },
+};
