b847b554fa8f64925a6d1a1a8086084f2ff64556
dogged <uzoechijerry@gmail.com>
2025-04-14 09:08:56 +0100

    tellme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -64,7 +64,7 @@ export default function Home() {
         </div>
 
         {/* this is the ther one */}
-        <BackgroundBeamsDemo />
+        {/* <BackgroundBeamsDemo /> */}
 
         {/* licensed investment adviser */}
 
